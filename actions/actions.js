3df476ef3302520127f9b583fd4704fe8b5277b3
Shashwat Mittal <shashwat.mittal5@gmail.com>
2017-04-20 15:54:35 +0530

    Adds isLoading to state, needs to implement pages, add pagination support

--- actions/actions.js
+++ actions/actions.js
@@ -1,36 +1,48 @@
 import {
-  RECEIVE_POSTS, RECEIVE_POST, RECEIVE_PAGE,REQUEST_POSTS,POSTS_PER_PAGE,
-  GET_TAGS, GET_CATEGORIES, GET_SETTINGS, REQUEST_POST,RECEIVE_MENU
+  RECEIVE_POSTS,
+  RECEIVE_POST,
+  REQUEST_POSTS,
+  REQUEST_POST,
+  RECEIVE_PAGES,
+  RECEIVE_PAGE,
+  REQUEST_PAGES,
+  REQUEST_PAGE,
+  POSTS_PER_PAGE,
+  GET_TAGS,
+  GET_CATEGORIES,
+  GET_SETTINGS,
+  RECEIVE_MENU,
+  TOTAL_PAGES_FOR_POSTS
 } from '../constants/constants';
 import fetch from 'isomorphic-fetch';
 
-function requestPost(id){
+function requestPost(id, loading){
   return{
     type: REQUEST_POST,
-    postID: id
+    id: id,
+    loading: loading
   }
 }
 
 function receivePost(id, json){
   return{
     type: RECEIVE_POST,
-    post: json,
-    postID: id
+    post: json
   }
 }
 
-function requestPosts(page){
+function requestPosts(currentPage, loading){
   return{
     type: REQUEST_POSTS,
-    page
+    currentPage: currentPage,
+    loading: loading
   }
 }
 
-function receivePosts(page, json){
+function receivePosts(pageNum, json){
   return{
     type: RECEIVE_POSTS,
     posts: json,
-    page
   }
 }
 
@@ -42,22 +54,65 @@ function receiveMenu (menuLocation, json){
   }
 }
 
+function receivePages(pages, json){
+  return{
+    type:RECEIVE_PAGES,
+    pages: json,
+  }
+}
+
+function requestPage(id, loading){
+  return{
+    type:REQUEST_PAGE,
+    pageID: id,
+    loading: loading
+  }
+}
+
+function receivePage(id, json){
+  return{
+    type:RECEIVE_PAGE,
+    page: json,
+    pageID: id
+  }
+}
+
+function noOfPagesforPosts(number){
+  return{
+    type: TOTAL_PAGES_FOR_POSTS,
+    noOfPages: number
+  }
+}
+
 export function fetchPost(id){
   return dispatch => {
-    dispatch(requestPost(id));
+    dispatch(requestPost(id, true));
     return fetch('http://abc.shashwatmittal.com/wp-json/wp/v2/posts/'+id)
       .then(response => response.json())
       .then(json => dispatch(receivePost(id, json)));
   }
 }
 
-export function fetchPosts(page){
+export function fetchPosts(currentPage){
   return dispatch => {
-    dispatch(requestPosts(page));
-    return fetch('http://abc.shashwatmittal.com/wp-json/wp/v2/posts?per_page='+POSTS_PER_PAGE+'&page='+page)
-      .then(response => response.json())
-      .then(json => dispatch(receivePosts(page, json)));
+    dispatch(requestPosts(currentPage, true));
+    return fetch('http://abc.shashwatmittal.com/wp-json/wp/v2/posts?per_page='+POSTS_PER_PAGE+'&page='+currentPage)
+      .then(function(response){
+        dispatch(noOfPagesforPosts(response.headers.get('X-WP-TotalPages')));
+        return response.json();
+      })
+      .then(json => dispatch(receivePosts(currentPage, json)));
   }
+}
+
+export function fetchPages(){
+  return dispatch => {
+    dispatch(requestPages(true))
+  }
+}
+
+export function fetchPage(){
+
 }
 
 export function fetchMenu(menuLocation){
