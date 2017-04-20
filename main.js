ad5795f6ba962a2ae9e71a01337d69c2ef9c3c43
Shashwat Mittal <shashwat.mittal5@gmail.com>
2017-04-20 16:00:44 +0530

    Updates project description

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ import { createLogger } from 'redux-logger'
 import {BrowserRouter as Router, Route ,browserHistory, Link, Switch} from 'react-router-dom';
 import Home from './layouts/Home'
 import Archive from './layouts/Archive'
+import Pages from './layouts/Pages';
+import Single from './components/Single';
 import reducer from './reducers/reducers'
 import {fetchPosts, fetchPost, fetchMenu} from './actions/actions'
 // Displaying Components for Testing Purposes.
@@ -18,21 +20,20 @@ const initialState = {
 }
 let store = createStore(reducer, initialState, applyMiddleware(thunkMiddleware, logger));
 
-
-store.dispatch(fetchPost('79'));
-
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory}>
       <div>
         <Link to='/wpReactTheme/'>Home</Link>
-        <Link to='/wpReactTheme/archive'>Archive</Link>
-
+        <Link to='/wpReactTheme/archive/p/1'>Archive</Link>
+        <Link to='/wpReactTheme/pages'>Pages</Link>
         <hr />
 
         <Switch>
           <Route exact path='/wpReactTheme/' component={Home}/>
-          <Route path='/wpReactTheme/archive' component={Archive}/>
+          <Route path='/wpReactTheme/archive/p/:page' component={Archive}/>
+          <Route path='/wpReactTheme/pages' component={Pages}/>
+          <Route exact path='/wpReactTheme/archive/:id' component={Single}/>
         </Switch>
       </div>
     </Router>
