c79ebd1dfa4913575b38b46a71801024c931b7da
Shashwat Mittal <shashwat.mittal5@gmail.com>
2017-04-28 04:58:59 +0530

    Improves pagination

--- components/componentParts/Pagination.js
+++ components/componentParts/Pagination.js
@@ -5,27 +5,28 @@ class Pagination extends Component{
   render(){
 
     let NavLinks;
-    const {path, currentPage, noOfPages} = this.props
+    const {currentPage, noOfPages, url} = this.props
+    console.log();
     let next = (parseInt(currentPage)+1)
     let previous = parseInt(currentPage)-1
-    let parentPath = path.split(":", 1)
+    let parentPath = url.slice(0, url.length - 1)
     if(currentPage > 1 && currentPage < noOfPages){
       NavLinks =
       <div className='nav-links text-center'>
-        <Link to={parentPath[0]+previous} className='page-numbers col-xs-4 col-sm-4 col-md-4 col-lg-4 btn btn-primary'>Previous</Link>
+        <Link to={parentPath+previous} className='page-numbers col-xs-4 col-sm-4 col-md-4 col-lg-4 btn btn-primary'>Previous</Link>
         <span className='page-numbers current col-xs-4 col-sm-4 col-md-4 col-lg-4'><span className='meta-nav screen-reader-text'>Page </span>{currentPage}</span>
-        <Link to={parentPath[0]+next} className='page-numbers col-xs-4 col-sm-4 col-md-4 col-lg-4 btn btn-primary'>Next</Link>
+        <Link to={parentPath+next} className='page-numbers col-xs-4 col-sm-4 col-md-4 col-lg-4 btn btn-primary'>Next</Link>
       </div>
     }else if(currentPage == 1 && currentPage < noOfPages){
       NavLinks =
       <div className='nav-links text-center'>
       <span className='page-numbers col-xs-offset-4 col-sm-offset-4 col-md-offset-4 col-lg-offset-4 col-xs-4 col-sm-4 col-md-4 col-lg-4'><span className='meta-nav screen-reader-text'>Page </span>{currentPage}</span>
-      <Link to={parentPath[0]+next} className='page-numbers col-xs-4 col-sm-4 col-md-4 col-lg-4 btn btn-primary'>Next</Link>
+      <Link to={parentPath+next} className='page-numbers col-xs-4 col-sm-4 col-md-4 col-lg-4 btn btn-primary'>Next</Link>
       </div>
     }else if(currentPage > 1 && currentPage == noOfPages){
       NavLinks =
       <div className='nav-links text-center'>
-      <Link to={parentPath[0]+previous} className='page-numbers col-xs-4 col-sm-4 col-md-4 col-lg-4 btn btn-primary'>Previous</Link>
+      <Link to={parentPath+previous} className='page-numbers col-xs-4 col-sm-4 col-md-4 col-lg-4 btn btn-primary'>Previous</Link>
       <span className='page-numbers col-xs-4 col-sm-4 col-md-4 col-lg-4'><span className='meta-nav screen-reader-text'>Page </span>{currentPage}</span>
       </div>
     }
