35c3ac938d50da6f3f1ece1da80c24c36612f7f0
Shashwat Mittal <shashwat.mittal5@gmail.com>
2017-05-02 05:05:27 +0530

    Adds support for required plugins.

--- functions.php
+++ functions.php
@@ -1,57 +1,80 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+require_once 'class-tgm-plugin-activation.php';
+
+/**
+ * Enqueues the required scripts for the theme.
  */
-
-function enqueue_scripts(){
-    wp_register_script('react_theme' , 'http://localhost:8080/index.js', array(), NULL , true);
-    $translation_array = array(
-        'errorPage' => array(
-                    'title' => __( 'Oops! That page can\'t be found.', 'blackhawk' ),
-                    'content'=> __('It looks like nothing was found at this location. Maybe try a search?', 'blackhawk')
-        ),
-        'searchForm' => array(
-            'action' => esc_url( home_url( '/' ) ),
-            'screen_reader_text' => _x( 'Search for:', 'label', 'blackhawk' ),
-            'placeholder' => esc_attr_x( 'Search ...', 'placeholder', 'blackhawk' ),
-            'value' => get_search_query(),
-            'searchButton' => _x( 'Search', 'submit button', 'blackhawk' )
-        ),
-        'constants' => array(
-            'SITE_TITLE' => get_bloginfo('name'),
-            'SITE_DESCRIPTION'=> get_bloginfo('description'),
-            'SITE_URL' => get_bloginfo('url'),
-            'WP_VERSION' => get_bloginfo('version'),
-            'API' => '/wp-json/wp/v2/',
-            'CUSTOM_MENU_API' => '/wp-json/wp-api-menus/v2/',
-            'POSTS_PER_PAGE' => get_option('posts_per_page'),
-            'MENUS_API' => 'wp-json/wp-api-menus',
-            'PERMALINK_STRUCTURE' => get_option('permalink_structure'),
-            'FRONT_PAGE' => get_option('page_on_front')
-        )
-    );
-    wp_localize_script('react_theme', 'phpData' , $translation_array);
-    wp_enqueue_script( 'react_theme' );
-    wp_enqueue_style('react_theme_css', trailingslashit(get_template_directory_uri()).'style.css', NULL);
+function enqueue_scripts() {
+	wp_register_script( 'react_theme', trailingslashit( get_template_directory_uri() ) . 'assets/js/index.js', array(), NULL, true );
+	wp_register_style( 'bootstrap_css', trailingslashit( get_template_directory_uri() ) . 'assets/css/bootstrap.min.css', NULL );
+	$translation_array = array(
+		'constants' => array(
+			'SITE_TITLE'		 => get_bloginfo( 'name' ),
+			'SITE_DESCRIPTION'	 => get_bloginfo( 'description' ),
+			'SITE_URL'			 => get_bloginfo( 'url' ),
+			'WP_VERSION'		 => get_bloginfo( 'version' ),
+			'API'				 => '/wp-json/wp/v2/',
+			'MENU_API'			 => '/wp-json/wp-api-menus/v2/',
+			'POSTS_PER_PAGE'	 => get_option( 'posts_per_page' ),
+			'FRONT_PAGE'		 => get_option( 'page_on_front' )
+		)
+	);
+	wp_localize_script( 'react_theme', 'phpData', $translation_array );
+	wp_enqueue_script( 'react_theme' );
+	wp_enqueue_style( 'react_theme_css', trailingslashit( get_template_directory_uri() ) . 'style.css', ['bootstrap_css' ] );
 }
 
-add_action('wp_enqueue_scripts', enqueue_scripts);
+add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
 
-function blackhawk_setup(){
-    register_nav_menus( array(
-  		'primary' => __( 'Primary Menu', 'blackhawk' ),
-  		'social'  => __( 'Social Links Menu', 'blackhawk' ),
-  	));
+/**
+ * Registering Navigation Menu and Sidebar for the theme.
+ */
+function blackhawk_setup() {
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu', 'blackhawk' ),
+	) );
 
-    register_sidebar( array(
-      'name' => __('Sidebar', 'blackhawk'),
-      'description' => __('Sidebar to Display all the Widgets', 'blackhawk'),
-      'before_widget' => '<section id="%1$s" class="widget %2$s clearfix">',
-      'after_widget' => '</section>'
-    ));
-
+	register_sidebar( array(
+		'name'			 => __( 'Sidebar', 'blackhawk' ),
+		'id'			 => 'sidebar-1',
+		'description'	 => __( 'Sidebar to Display all the Widgets', 'blackhawk' ),
+		'before_widget'	 => '<section id="%1$s" class="widget %2$s clearfix">',
+		'after_widget'	 => '</section>'
+	) );
 }
-add_action('after_setup_theme',  blackhawk_setup);
+
+add_action( 'after_setup_theme', 'blackhawk_setup' );
+
+/**
+ * Prompts for downloading and installing the required plugins on which the theme depends upon.
+ */
+function blackhawk_register_required_plugins() {
+	$plugins = array(
+		array(
+			'name'		 => 'WP Rest API Sidebars',
+			'slug'		 => 'wp-rest-api-sidebars',
+			'required'	 => true,
+		),
+		array(
+			'name'		 => 'WP API Menus',
+			'slug'		 => 'wp-api-menus',
+			'required'	 => true,
+		),
+	);
+
+	$config = array(
+		'id'			 => 'blackhawk', // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path'	 => '', // Default absolute path to bundled plugins.
+		'menu'			 => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'	 => 'themes.php', // Parent menu slug.
+		'capability'	 => 'edit_theme_options', // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'	 => true, // Show admin notices or not.
+		'dismissable'	 => true, // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'	 => '', // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic'	 => false, // Automatically activate plugins after installation or not.
+		'message'		 => '', // Message to output right before the plugins table.
+	);
+	tgmpa($plugins, $config);
+}
+add_action( 'tgmpa_register', 'blackhawk_register_required_plugins' );
